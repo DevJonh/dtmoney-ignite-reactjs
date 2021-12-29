08acb0369040ef477af8664beeb9ef4e4d82b73f
Jhon Senna <jhonnyoliveira.jhon@gmail.com>
2021-12-29 13:34:26 -0300

    header component finalization

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Header } from "./components/Header";
+
 export function App() {
   return (
-    <div className="App">
+    <>
+      <Header />
       <h1>Hello World</h1>
-    </div>
+    </>
   );
 }
